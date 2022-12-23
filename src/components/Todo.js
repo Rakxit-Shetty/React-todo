5497c2d82ff1993fd63d20bc471a149655dc2206
Rakxit-Shetty <Rakshitshetty136@gmail.com>
2022-12-23 20:18:01 +0530

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react'
+import '../CSS/style.css'
+export default function Todo() {
+    
+    const[input,setInput]=useState("");
+    const[item,setItem]=useState([])
+
+    const addItem=()=>{
+      if(!input)
+      {
+
+      }
+      else{
+      setItem([...item,input])
+      setInput('')
+      }
+    }
+    
+    
+    const delitem = (id) => {
+      const updateditems = item.filter((elem,ind) => {
+          return ind !== id;
+      });
+
+      setItem(updateditems);
+  }
+
+  //remove all
+  const removeAll = ()=>{
+    setItem([]);
+  }
+
+  return (
+
+    <>
+    <div className="main-div" >
+    <div className="child-div">
+    <figure>
+    <figcaption> Add Your List Here </figcaption>
+    
+    </figure>
+                        
+                        
+  
+                    <div className="addItems">
+                        <input type="text" placeholder="Add Items..."
+                           value={input} 
+                           onChange={(e) => setInput(e.target.value) }
+                        />
+                      <i className="fa fa-plus add-btn"  onClick={addItem}></i>
+                    </div>
+
+
+  
+                    <div className="showItems">
+      <div>
+      {
+        item.map((ele,ind)=>{
+        return(
+            <div className="eachItem" key={ind}>
+              <h1>{ele}</h1>
+              <i className="far fa-trash-alt add-btn" title='delete' onClick={() => delitem(ind)}></i>
+              
+            </div>
+          )
+        })
+      }
+</div>
+         <button className="btn effect04" data-sm-link-text="REMOVE ALL" onClick={removeAll }><span>CHECK LIST</span></button>
+      </div>
+     
+    </div>
+       
+      </div>
+      
+    </>
+    
+  )
+}
